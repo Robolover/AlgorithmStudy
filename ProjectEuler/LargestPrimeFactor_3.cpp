90466782a2f484c09d42df60d64cfafd39c953c0
Bogyeong Kim <rlaqhrud852@gmail.com>
2019-06-24 22:19:48 +0900

    largest prime factor

---
+++ ProjectEuler/LargestPrimeFactor_3.cpp
@@ -0,0 +1,43 @@
+/*--------------------[3] Largest prime factor-----------------
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143 ?
+
+solved 2019. 06. 24. mon
+--------------------------------------------------------------*/
+
+#include <iostream>
+
+void get_prime_factor(__int64 num);
+
+int main(void)
+{
+    __int64 number;
+    std::cout << "input number : " ;
+    std::cin >> number;
+
+    get_prime_factor(number); 
+    return 0;
+}
+
+void get_prime_factor(__int64 num)
+{
+    __int64 i = 2;
+    while (true)
+    {
+        if(num % i == 0)
+        {
+            std::cout << i << " ";
+            num /= i;
+            std::cout << num << std::endl;
+            get_prime_factor(num);
+            break;
+        }
+
+        else if(num < 2)
+        {
+            break;
+        }
+
+        else i++;
+    }
+}
